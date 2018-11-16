2ae289c1084603f902e8483febbd22084c163bf6
kaigouthro <kaigouthro@gmail.com>
2018-11-16 09:38:43 -0700

    Revert "Update ChatFeathers.js"
    This reverts commit a93e6335c9feefdd07e2f2dcdea283ec859bacf2.

--- src/containers/ChatFeathers/ChatFeathers.js
+++ src/containers/ChatFeathers/ChatFeathers.js
@@ -4,17 +4,7 @@ import { provideHooks } from 'redial';
 import { connect } from 'react-redux';
 import reducer, * as chatActions from 'redux/modules/chat';
 import { withApp } from 'hoc';
-import {
-  Button,
-  Segment,
-  Form,
-  Label,
-  Input,
-  Icon,
-  Header,
-  Container,
-  Message
-} from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 
 @provideHooks({
   fetch: async ({ store: { dispatch, getState, inject } }) => {
@@ -25,12 +15,12 @@ import {
     if (state.online) {
       return dispatch(chatActions.load()).catch(() => null);
     }
-  },
+  }
 })
 @connect(
   state => ({
     user: state.auth.user,
-    messages: state.chat.messages,
+    messages: state.chat.messages
   }),
   { ...chatActions }
 )
@@ -38,18 +28,18 @@ import {
 class ChatFeathers extends Component {
   static propTypes = {
     app: PropTypes.shape({
-      service: PropTypes.func,
+      service: PropTypes.func
     }).isRequired,
     user: PropTypes.shape({
-      email: PropTypes.string,
+      email: PropTypes.string
     }).isRequired,
     addMessage: PropTypes.func.isRequired,
-    messages: PropTypes.arrayOf(PropTypes.object).isRequired,
+    messages: PropTypes.arrayOf(PropTypes.object).isRequired
   };
 
   state = {
     message: '',
-    error: null,
+    error: null
   };
 
   componentDidMount() {
@@ -57,18 +47,14 @@ class ChatFeathers extends Component {
   }
 
   componentWillUnmount() {
-    this.props.app
-      .service('messages')
-      .removeListener('created', this.props.addMessage);
+    this.props.app.service('messages').removeListener('created', this.props.addMessage);
   }
 
   handleSubmit = async event => {
     event.preventDefault();
 
     try {
-      await this.props.app
-        .service('messages')
-        .create({ text: this.state.message });
+      await this.props.app.service('messages').create({ text: this.state.message });
       this.setState({ message: '', error: false });
     } catch (error) {
       console.log(error);
@@ -79,48 +65,40 @@ class ChatFeathers extends Component {
   render() {
     const { user, messages } = this.props;
     const { error } = this.state;
+
     return (
-      <Container>
-        <Segment raised color="blue">
-          <Header as="h3">
-            <Icon name="chat" />
-            Chat with Feathers.
-          </Header>
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Field inline>
-              <Label>Message</Label>
-              <Input
+      <div className="container">
+        <h1>Chat</h1>
+
+        {user && (
+          <div>
+            <ul>
+              {messages.map(msg => (
+                <li key={`chat.msg.${msg._id}`}>
+                  {msg.sentBy.email}
+:
+                  {msg.text}
+                </li>
+              ))}
+            </ul>
+            <form onSubmit={this.handleSubmit}>
+              <input
                 type="text"
                 ref={c => {
                   this.message = c;
                 }}
                 placeholder="Enter your message"
                 value={this.state.message}
-                onChange={event => this.setState({ message: event.target.value })
-                }
+                onChange={event => this.setState({ message: event.target.value })}
               />
-              <Button onClick={this.handleSubmit} icon="send" content=" Send" />
-              {error && (
-                <Label as={error}>
-                  <Icon name="cancel" />
-                  {error}
-                </Label>
-              )}
-            </Form.Field>
-          </Form>
-        </Segment>
-        {user && (
-          <Segment color="teal" raised style={{ overflow: 'auto', maxHeight: '65vh' }}>
-            {messages.slice(0).reverse().map(msg => (
-              <Message key={`chat.msg.${msg._id}`} raised size="tiny">
-                {msg.sentBy.email}
-                :&nbsp;&nbsp;
-                {msg.text}
-              </Message>
-            ))}
-          </Segment>
+              <Button onClick={this.handleSubmit}>
+                Send
+              </Button>
+              {error && <div className="text-danger">{error}</div>}
+            </form>
+          </div>
         )}
-      </Container>
+      </div>
     );
   }
 }
