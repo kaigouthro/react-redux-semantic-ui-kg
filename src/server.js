c589221ac1361980e5fe293e4a806341460ed821
kaigouthro <kaigouthro@gmail.com>
2018-11-19 17:27:16 -0700

    full move to Yarn, esdocumentation install
    Signed-off-by: kaigouthro <kaigouthro@gmail.com>

--- src/server.js
+++ src/server.js
@@ -173,8 +173,8 @@ app.use(async (req, res) => {
       if (err) {
         console.error(err);
       }
-      console.info('----\n==> ✅  %s is running, talking to API server on %s.', config.app.title, config.apiPort);
-      console.info('==> 💻  Open http://%s:%s in a browser to view the app.', config.host, config.port);
+      console.info('----\n==> ?  %s is running, talking to API server on %s.', config.app.title, config.apiPort);
+      console.info('==> ??  Open http://%s:%s in a browser to view the app.', config.host, config.port);
     });
   } else {
     console.error('==>     ERROR: No PORT environment variable has been specified');
