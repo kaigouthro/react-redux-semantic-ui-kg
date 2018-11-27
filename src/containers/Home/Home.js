d5c2f61e6a08250b1caf562d0d5e52bb91929101
kaigouthro <kaigouthro@gmail.com>
2018-11-26 17:46:54 -0700

    >.<
    Signed-off-by: kaigouthro <kaigouthro@gmail.com>

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { CounterButton } from 'components';
 import config from 'config';
 import Helmet from 'react-helmet';
